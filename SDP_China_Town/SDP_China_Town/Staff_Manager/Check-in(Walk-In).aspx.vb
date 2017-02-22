decf4f90778f12a36132d46dede4555aa0f20b9e
DLim97 <dapelim8@gmail.com>
2017-02-22 22:33:11 +0800

    New SDP
    blablabla

---
+++ SDP_China_Town/SDP_China_Town/Staff_Manager/Check-in(Walk-In).aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class WebForm9
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim connect As String = "Provider=Microsoft.Jet.OleDb.4.0;" & "Data Source=|DataDirectory|Hote.mdb"
+        Dim query As String = ("INSERT INTO CheckInOut (CheckInOutID , CheckIn, CheckOut, CustomerID, RoomID, ReservationID) VALUES (id , '" & TextBox1.Text & "', '" & Calendar1.TodaysDate.ToShortDateString & "','" & Calendar1.SelectedDate.ToShortDateString & "', NULL, '" & DropDownList1.SelectedValue & "', NULL")
+        Dim con As New OleDbConnection(connect)
+        Dim cmd As New OleDbCommand(query, con)
+
+        con.Open()
+
+        cmd.ExecuteNonQuery()
+
+
+    End Sub
+End Class
